b9585fd43b3803508259b773189ac157b73fd641
TheBlay <trap02.mar@hotmail.com>
2018-08-09 02:35:38 +0900

    Alterado o modo de scripting, agora com jQuery

---
+++ www/components/calculo.js
@@ -0,0 +1,18 @@
+// This is a JavaScript file
+
+alert("MINGAU");
+
+$(document).on("click", "#somar", function(){
+    
+    alert("PIROCA");
+    
+            var x1 = parseInt( $("#valor1").val());
+            var x2 = parseInt( $("#valor2").val());
+            
+    alert("CU");
+            
+            var resX = x1 + x2;
+            $("#res").val(resX);
+            
+    
+    });
