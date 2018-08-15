c87b5e05fc8e7d7a36bc911ed735586492df8987
TheBlay <trap02.mar@hotmail.com>
2018-08-16 03:05:38 +0900

    Criados os demais botões, ainda sem função

--- www/components/calculo.js
+++ www/components/calculo.js
@@ -1,15 +1,15 @@
 // This is a JavaScript file
 
-alert("MINGAU");
+
 
 $(document).on("click", "#somar", function(){
     
-    alert("PIROCA");
+   
     
             var x1 = parseInt( $("#valor1").val());
             var x2 = parseInt( $("#valor2").val());
             
-    alert("CU");
+   
             
             var resX = x1 + x2;
             $("#res").val(resX);
